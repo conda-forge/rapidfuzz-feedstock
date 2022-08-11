20dd71472b9e6a7504af3998cfbdcb075c855007
ngam <67342040+ngam@users.noreply.github.com>
2022-08-11 12:28:19 -0400

    Create build_osxarm.sh

---
+++ recipe/build_osxarm.sh
@@ -0,0 +1,8 @@
+export CMAKE_OSX_ARCHITECTURES="arm64"
+# workaround for https://github.com/scikit-build/scikit-build/issues/589
+rm -rf $PREFIX/lib/libpython$PY_VER.dylib
+ln -sf $PREFIX/lib/libc++.dylib $PREFIX/lib/libpython$PY_VER.dylib
+export SKBUILD_CONFIGURE_OPTIONS=$(echo $CMAKE_ARGS | sed -e 's/\-DCMAKE_INSTALL_PREFIX\=[^ ]* //g')
+
+
+{{ PYTHON }} -m pip install . -vvv
